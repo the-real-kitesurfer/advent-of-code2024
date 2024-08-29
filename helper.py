778542b22da40eaa5ebea278350b7f9383182ddf
Agnieszka Pasieka <61354571+agapasieka@users.noreply.github.com>
2024-08-29 20:28:15 +0100

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,37 @@
+import csv
+# Reads a CSV file and returns a list of dictionaries
+def read_data():
+    data = []
+    with open("Olympics_2024.csv", "r", encoding="utf-8-sig") as csvfile:
+        spreadsheet = csv.DictReader(csvfile)
+        for row in spreadsheet:
+            data.append(row)
+    return data
+data = read_data()
+
+# Calculates total medals per country
+def calculate_total_medals():
+    total_medals_per_country = {}  # Defines variables for dictionaries
+    for row in data:
+        country = row["NOC"]
+        total_medals = int(row["Total"])   # Convert the total value to integer
+        if country not in total_medals_per_country:
+            total_medals_per_country[country] = 0
+        total_medals_per_country[country] += total_medals  # Sum the medals
+    return total_medals_per_country
+
+# Displays total number of medals per country from csv
+results = calculate_total_medals()
+print("Total Medals Per Country:\n")
+for country, total_medals in results.items():
+    print(f'{country}: {total_medals}')
+
+
+# Filters the data based on the country name entered by user
+def filter_data(user_input):
+    filter_data_per_country = []
+    for row in data:
+        country = row['NOC'].strip().lower()
+        if country == user_input.lower():
+            filter_data_per_country.append(row)
+    return filter_data_per_country

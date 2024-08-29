20580562c7c6cb8deaa845eeffcba2feb259ee5e
Agnieszka Pasieka <61354571+agapasieka@users.noreply.github.com>
2024-08-29 20:32:05 +0100

    Add files via upload

---
+++ total_medals_with_user_input.py
@@ -0,0 +1,20 @@
+import pprint
+import helper
+
+data = helper.read_data()
+
+# Displays total number of medals per country from csv
+results = helper.calculate_total_medals()
+print("Total Medals Per Country:\n")
+for country, total_medals in results.items():
+    print(f'{country}: {total_medals}')
+
+
+user_input = ""
+while user_input != "exit":
+    user_input = input("Enter the country to view olympic details (or 'exit' to quit):\n")
+    if user_input == "exit":
+        break
+    filtered_data = helper.filter_data(user_input)
+
+    pprint(filtered_data)

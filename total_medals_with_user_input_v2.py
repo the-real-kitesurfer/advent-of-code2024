bed50ad3781fd1319d2fc0b11ffb8faef2b75d0d
mbroya <mbroyall@gmail.com>
2024-09-01 12:57:35 +0100

    Update total_medals_with_user_input_v2.py

--- total_medals_with_user_input_v2.py
+++ total_medals_with_user_input_v2.py
@@ -6,6 +6,7 @@ print("Welcome to the Group 5 Olympics Spreadsheet Analysis Project!")
 print("Please pick an option from the following menus:")
 print("\n'Total' for total medals won across all competing countries")
 print("'Country' to enable user input to view medals won by a specific country")
+print("'Top' to view the top ten scoring countries in descending order")
 
 menu_choice = input("\nPlease enter which menu you would like: ")
 if menu_choice.lower() == "total":

b7b4c7bd48c8962ac0d815c68d153bc596330795
mbroya <mbroyall@gmail.com>
2024-09-01 12:12:01 +0100

    Create total_medals_with_user_input_v2.py
    With starter menu

---
+++ total_medals_with_user_input_v2.py
@@ -0,0 +1,16 @@
+import helper_v2
+
+data = helper_v2.read_data()
+
+print("Welcome to the Group 5 Olympics Spreadsheet Analysis Project!")
+print("Please pick an option from the following menus:")
+print("\n'Total' for total medals won across all competing countries")
+print("'Country' to enable user input to view medals won by a specific country")
+
+menu_choice = input("\nPlease enter which menu you would like: ")
+if menu_choice.lower() == "total":
+    helper_v2.total_medals()
+elif menu_choice.lower() == "country":
+    helper_v2.country_details()
+else:
+    print("Input not recognised, please try again.")

e468ce47362680f02edce0de015fa4921a554344
the-real-kitesurfer <149383130+the-real-kitesurfer@users.noreply.github.com>
2024-12-10 07:08:09 +0000

    Moving sample data and using central helper to fetch data

--- src/helper.py
+++ src/helper.py
@@ -3,3 +3,17 @@ DEBUG = True
 def debug(msg):
     if DEBUG:
         print(msg)
+
+def fetchData(day, realData):
+    if realData:
+        filename = "./resources/data/day" + str(day) + ".txt"    
+    else:
+        filename = "./resources/samples/sample" + str(day) + ".txt"
+
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        listOfStrings=[]
+        for i, line in enumerate(lines):
+            listOfStrings.append(line[:-1])
+    
+    return listOfStrings

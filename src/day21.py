fc4514fac066ad593920313d64dbd96a362efc36
real-kitesufer <the-real-kitesurfer@proton.me>
2024-12-27 16:01:57 +0000

    Part 2 likely solved, but too slow/big for 25 rounds

--- src/day21.py
+++ src/day21.py
@@ -31,21 +31,21 @@ def fromCoordinate(pos, isDoor):
   for key in coordinates:
     if coordinates[key] == pos:
       return key
-  print(f"Did not find key for {pos} (onDoor: {isDoor})")
+  debug(f"Did not find key for {pos} (onDoor: {isDoor})")
   return "?"
 
-def validate(typed, keypad, inFocus):
-  focusedPos = coordinate(inFocus, keypad == 1)
+def validate(typed, isFirstKeypad, inFocus):
+  focusedPos = coordinate(inFocus, isFirstKeypad)
   for i, c in enumerate(typed):
     if c == '<': focusedPos = (focusedPos[0] - 1, focusedPos[1])
     if c == '>': focusedPos = (focusedPos[0] + 1, focusedPos[1])
     if c == '^': focusedPos = (focusedPos[0], focusedPos[1]-1)
     if c == 'v': focusedPos = (focusedPos[0], focusedPos[1]+1)
-    keyInFocus = fromCoordinate(focusedPos, keypad == 1)
+    keyInFocus = fromCoordinate(focusedPos, isFirstKeypad)
     if keyInFocus == '?':
-      print(f"Lost focus when typing character {i} ({c}) from {typed} with focus on {inFocus} on {keypad}")
-      return False
-  return True
+      debug(f"Lost focus when typing character {i} ({c}) from {typed} with focus on {inFocus} on isFirstKeypad: {isFirstKeypad}")
+      return keyInFocus
+  return keyInFocus
 
 @lru_cache(maxsize=None)
 def coordinate(c, isDoor):
@@ -92,10 +92,33 @@ def deltaToKeys(x,y, xFirst):
     +2: "vv",
     +3: "vvv"
   }    
-  return [keysForX[x] + keysForY[y] + 'A', keysForY[y] + keysForX[x] + 'A']
+  #return [keysForX[x] + keysForY[y] + 'A', keysForY[y] + keysForX[x] + 'A']
+  if xFirst:
+    return keysForX[x] + keysForY[y] + 'A'
+  else:
+    return keysForY[y] + keysForX[x] + 'A'
 
 @lru_cache(maxsize=None)
-def findRoutes(c1, c2, keypad):
+def findRoutes(c1, c2, isFirstKeypad):
+  pos1, pos2 = coordinate(c1, isFirstKeypad), coordinate(c2, isFirstKeypad)
+  # try to find optimal order without options -> maybe by identifying blocks: best solution for each current,next pair across 3 or more levels!?
+  if isFirstKeypad: #the 1x9 grid on the final door
+    xFirst = pos1[0] > pos2[0]
+    if c1 in ['0', 'A'] and c2 in ['7', '4', '1']: #make sure to avoid the void by FIRST going up and then left
+      xFirst = False
+    if c1 in ['7', '4', '1'] and c2 in ['0', 'A']: #make sure to avoid the void by FIRST going right and then down
+      xFirst = True
+  else: # the <^v> grid on one of the robots
+    xFirst = pos1[0] < pos2[0]
+    if c1 in ['^', 'A'] and c2 == '<': #make sure to avoid the void by FIRST going down and then left
+      xFirst = False
+    if c1 in ['<'] and c2 in ['^', 'A']: #make sure to avoid the void by FIRST going right and then up
+      xFirst = True
+
+  #debug(f"{pos1} -> {pos2} = {deltaToKeys(pos2[0] - pos1[0], pos2[1] - pos1[1], xFirst)} with xFirst = {xFirst}")
+  return deltaToKeys(pos2[0] - pos1[0], pos2[1] - pos1[1], xFirst)
+
+def findRoutesOLD(c1, c2, keypad):
   options = []
   for defaultXFirst in [True, False]:
     xFirst = defaultXFirst
@@ -114,67 +137,49 @@ def findRoutes(c1, c2, keypad):
     #debug(f"{pos1} -> {pos2} = {deltaToKeys(pos2[0] - pos1[0], pos2[1] - pos1[1], xFirst)} with xFirst = {xFirst}")
     for option in deltaToKeys(pos2[0] - pos1[0], pos2[1] - pos1[1], xFirst):
       # check: use only VALID sequences here ...
-      if validate(option, keypad, c1):
+      if not validate(option, keypad, c1) == '?':
         options.append(option)
   return options
 
-def typeIn(code, keypad, doValidation, origInFocus):
+def typeIn(code, keypad, doValidation, origInFocus, directionalKeypads):
+  debug(f"Typing {code} on {keypad} with length {len(code)}")
   if keypad == 1:
     debug("")
 
-  options = [""]
+  toType = ""
   inFocus = origInFocus # 'A'
-  for c in code:
-    routesForC = []
-    for route in findRoutes(inFocus, c, keypad):
-      routesForC.append(route)
+  for i, c in enumerate(code):
+    if i % 10000000 == 0:
+      print(f"Finding routes for {i}th of {len(code)} chars on keypad {keypad} ({(100*i)//len(code)}%)")
+    toType += findRoutes(inFocus, c, keypad == 1)
+    #for route in findRoutes(inFocus, c, keypad == 1):
+    #  routesForC.append(route)
     inFocus = c
 
-    newOptions = []
-    if False:
-      bestNewLength = -1
-      for route in routesForC:
-        if bestNewLength == -1 or len(route) < bestNewLength:
-          bestNewLength = len(route)
 
-    for prevOption in options:
-      for route in routesForC:
-        if (True or len(route) == bestNewLength) and not prevOption + route in newOptions:
-          newOptions.append(prevOption + route)
-
-    options = newOptions
-    #debug(f" Need to type in {toTypePerXFirst[defaultXFirst]} for {code} on {keypad}, length {len(toTypePerXFirst[defaultXFirst])} with defaultXFirst: {defaultXFirst}")
-
-
-  debug(f" Need to type in any of {options} for {code} on {keypad}")
+  if DEBUG: debug(f" Need to type in {toType} for {code} on {keypad}")
   if doValidation:
-    for option in options:
-      if not validate(option, keypad, origInFocus):
-        print(f"Invalid sequence detected in {option} with focus on {origInFocus} on {keypad}")
-        return []
+    if validate(toType, keypad == 1, origInFocus) == '?':
+      print(f"Invalid sequence detected in {toType} with focus on {origInFocus} on {keypad}")
+      return []
 
-  if keypad == 3:
+  if keypad == directionalKeypads:
     if doValidation:
       if code == "029A":
         debug(f"Expected to type <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A, length {len("<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A")}")
       else:
         debug(f"Expected length: Code 1 = 68, Code 2 = 60, Code 3 = 68, Code 4 = 64, Code 5 = 64")
-    return options # toType
+    return [toType] # toType
   else:
     if code == "029A":
       if keypad == 2: debug(f"Expected to type v<<A>>^A<A>AvA<^AA>A<vAAA>^A, length {len("v<<A>>^A<A>AvA<^AA>A<vAAA>^A")}")
       if keypad == 1: debug(f"Expected to type <A^A>^^AvvvA, length {len("<A^A>^^AvvvA")}")
-    combinedOptions = []
-    for i, option in enumerate(options):
-      print(f"Checking option {i} of {len(options)}")
-      innerOptions = typeIn(option, keypad + 1, doValidation, 'A')
-      if innerOptions == []:
-        print(f"Invalid sequence found in {option}  with focus {'A'} on {keypad + 1}")
-        return []
-      for innerOption in innerOptions:
-        if not innerOption in combinedOptions:
-          combinedOptions.append(innerOption)
-    return combinedOptions
+
+    innerOptions = typeIn(toType, keypad + 1, doValidation, 'A', directionalKeypads)
+    if innerOptions == []:
+      print(f"Invalid sequence found in {toType}  with focus {'A'} on {keypad + 1}")
+      return []
+    return innerOptions
 
 def shortestOption(options):
   shortest = -1
@@ -183,6 +188,14 @@ def shortestOption(options):
     if shortest == -1 or len(option) < len(options[shortest]):
       shortest = i
   return options[shortest]
+
+def longestOption(options):
+  longest = -1
+  for i, option in enumerate(options):
+    debug(f"Option {i} of {len(options)} with length {len(option)}: {option}")
+    if longest == -1 or len(option) > len(options[longest]):
+      longest = i
+  return options[longest]
 
 def complexity(code, toType):
   numericPart = ""
@@ -198,15 +211,17 @@ def part1(useRealData):
   codes = fetchData(DAY, useRealData)
 
   totalComplexity = 0
+  directionalKeypads = 2+1
   for code in codes:
     inFocus = 'A'
-    shortest = ""
+    result = ""
     for c in code:
-      toType = typeIn(c, 1, True or not useRealData, inFocus)
+      toType = typeIn(c, 1, True or not useRealData, inFocus, directionalKeypads)
       inFocus = c
-      shortest += shortestOption(toType)
-    debug(f"Shortest option from {toType} is {shortest}")
-    totalComplexity += complexity(code, shortest)
+      shortest = shortestOption(toType)
+      debug(f"Shortest option from {toType} is {shortest}")
+      result += shortest
+    totalComplexity += complexity(code, result)
 
   print(f"Result for part 1: {str(totalComplexity)}")
   print(f"Expected:          126384")
@@ -216,14 +231,49 @@ def part2(useRealData):
 
   codes = fetchData(DAY, useRealData)
 
-  totalComplexity = typeCodes(codes, 4)
+  totalComplexity = 0
+  directionalKeypads = 25+1
+  for code in codes:
+    inFocus = 'A'
+    result = ""
+    #code = "<vA<AA>>^AAvA<^A>AvA^A"
+    for c in code:
+      toType = typeIn(c, 1, False or not useRealData, inFocus, directionalKeypads)
+      inFocus = c
+      shortest = shortestOption(toType)
+      shortestResult1 = reverse(shortest, 4, "A")
+      shortestResult2 = reverse(shortestResult1, 3, "A")
+      shortestResult3 = ""#reverse(shortestResult2, 2, "A")
+      longest = longestOption(toType)
+      longestResult1 = reverse(longest, 4, "A")
+      longestResult2 = reverse(longestResult1, 3, "A")
+      longestResult3 = ""#reverse(longestResult2, 2, inFocus)
+      debug(f"Shortest option from {toType} is {shortest}")
+      if len(shortest) < len(longest):
+        print(f"for {c}: {len(shortest)} < {len(longest)}: {shortest} vs {longest} -> {shortestResult1} vs {longestResult1} -> {shortestResult2} vs {longestResult2} -> {shortestResult3} vs {longestResult3}")
+      result += shortest
+    totalComplexity += complexity(code, result)
 
   print(f"Result for part 2: {str(totalComplexity)}")
 
+def reverse(option, keypad, inFocus):
+  result = ""
+  typed = ""
+  for c in option:
+    if c == 'A':
+      result += typed
+    else:
+      typed = validate(c, keypad == 1, inFocus)
+      #print(f"Typing {c} from {option} with focus on {inFocus} -> {typed}")
+      inFocus = typed
+  return result
+
 def solve():
+  # new approach - simple count groups, and their length on the next level
   seq = "<^<A"
   seq = "v<A<AA>>^AvAA^<A>Av<<A>>^AvA^Av<<A>>^AAv<A>A^A<A>Av<A<A>>^AAA<Av>A^A"
   #print(f"Permutations for {seq}: {permuteSequence(seq,3)}")
+  part1(False)
   part1(True)
   # attempt 1: 165516
   # attempt 2: 166048
@@ -237,6 +287,18 @@ def solve():
   #typeIn("A", 1, False, '9')
   #print(typeIn("<", 3, True, 'A'))
   #+shortestOption(typeIn("2", 1, True, '0'))+shortestOption(typeIn("9", 1, True, '2'))+shortestOption(typeIn("A", 1, True, '9')))
-  #part2(False)
+  part2(True)
+  return
+  toType = typeIn("4", 1, True or not useRealData, "3", 4)
+  shortest = shortestOption(toType)
+  longest = longestOption(toType)
+  debug(f"Shortest option from {toType} is {shortest} and longest is {longest}")
+  for option in toType:
+    result = reverse(option, 4, "A")
+    result2 = reverse(result, 3, "A")
+    result3 = reverse(result2, 2, "A")
+    result4 = reverse(result3, 1, "3")
+    print(f"{option} -> {result} -> {result2} -> {result3} ({len(option)} vs {len(result)} vs {len(result2)} vs {len(result3)})")
 
   # likely best to start from scratch :-o
+  # 
